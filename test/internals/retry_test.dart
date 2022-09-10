9d4136635c2a1e0846caf6625300bdc8bac848be
Rasul <sofiyevsr@gmail.com>
2022-09-10 12:31:06 +0400

    fix tests

--- test/internals/retry_test.dart
+++ test/internals/retry_test.dart
@@ -6,20 +6,41 @@ import '../utils/run_fake_async.dart';
 void main() {
   test("Function should be run given times before throwing", () {
     int count = 0;
-    Future<void> getData(bool fail) async {
+    Future<void> getData() async {
       count++;
       throw Error();
     }
 
     runFakeAsync((time) async {
       expect(
-        retryFuture(() => getData(false), maxDelay: 1000, maxAttempts: 3),
+        retryFuture(() => getData(), maxDelay: 1000, maxAttempts: 3),
         throwsA(isA<Error>()),
       );
       time.elapse(
         const Duration(seconds: 3),
       );
       expect(count, 3);
+    });
+  });
+
+  test("If onFail method returns true function should throw immediately", () {
+    int count = 0;
+    Future<void> getData() async {
+      count++;
+      throw Error();
+    }
+
+    runFakeAsync((time) async {
+      expect(
+        retryFuture(
+          () => getData(),
+          onFail: (_) => true,
+          maxDelay: 1000,
+          maxAttempts: 3,
+        ),
+        throwsA(isA<Error>()),
+      );
+      expect(count, 1);
     });
   });
 }
